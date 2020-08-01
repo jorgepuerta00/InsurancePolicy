0df4b0625e847b09c9e949ec9fa7af05da68133a
Jorge Puerta <jorge.puerta00@gmail.com>
2020-08-01 12:13:20 -0500

    implemented repository methods to status type

--- InsurancePolicy.Infrastructure.Data/Repositories/StatusTypeRepository.cs
+++ InsurancePolicy.Infrastructure.Data/Repositories/StatusTypeRepository.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace InsurancePolicy.Infrastructure.Data.Repositories
 {
-    class StatusTypeRepository
+    using Domain.Interfaces;
+    using Domain.Models;
+    using Data.Context;
+
+    public class StatusTypeRepository : IStatusTypeRepository
     {
+        public InsurancePolicyDbContext _context;
+        public StatusTypeRepository(InsurancePolicyDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<StatusType> GetStatusTypes()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
