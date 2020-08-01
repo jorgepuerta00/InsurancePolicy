4091ce36359ca5b219302de46977cded915691f3
Jorge Puerta <jorge.puerta00@gmail.com>
2020-08-01 12:04:00 -0500

    implemented repository methods to coverate type

---
+++ InsurancePolicy.Infrastructure.Data/Repositories/CoverageTypeRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsurancePolicy.Infrastructure.Data.Repositories
+{
+    class CoverageTypeRepository
+    {
+    }
+}
